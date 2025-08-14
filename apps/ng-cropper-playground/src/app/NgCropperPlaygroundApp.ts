27441998b18c523dfcc2e0303654a0016f4c4ecf
Daniel <daniel.g@sps.co.il>
2025-08-14 09:34:30 +0300

    feat: implement NgCropper component with dynamic CropperJS import and update styles

--- apps/ng-cropper-playground/src/app/NgCropperPlaygroundApp.ts
+++ apps/ng-cropper-playground/src/app/NgCropperPlaygroundApp.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NxWelcome } from './nx-welcome';
-import { NgCropper } from '@dg/ng-cropper';
+import { Component } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { NxWelcome } from "./nx-welcome";
+import { NgCropper } from "@dg/ng-cropper";
 
 @Component({
   imports: [NxWelcome, RouterModule, NgCropper],
-  selector: 'ngCropperPlaygroundApp',
-  templateUrl: './NgCropperPlaygroundApp.html',
-  styleUrl: './NgCropperPlaygroundApp.scss',
+  selector: "ngCropperPlaygroundApp",
+  templateUrl: "./NgCropperPlaygroundApp.html",
+  styleUrl: "./NgCropperPlaygroundApp.scss",
+  schemas: [],
 })
 export class NgCropperPlaygroundApp {
-  protected title = 'ngCropper-playground';
+  protected title = "NgCropper-playground";
 }
--- libs/ng-cropper/src/lib/NgCropper/NgCropper.ts
+++ libs/ng-cropper/src/lib/NgCropper/NgCropper.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, CUSTOM_ELEMENTS_SCHEMA, OnInit} from '@angular/core';
 
 @Component({
   selector: 'ngCropper',
@@ -6,5 +6,12 @@ import {ChangeDetectionStrategy, Component} from '@angular/core';
   templateUrl: './NgCropper.html',
   styleUrl: './NgCropper.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class NgCropper {}
+export class NgCropper implements OnInit {
+  
+  async ngOnInit() {
+    // Dynamic import to load CropperJS web components
+    await import('cropperjs');
+  }
+}
