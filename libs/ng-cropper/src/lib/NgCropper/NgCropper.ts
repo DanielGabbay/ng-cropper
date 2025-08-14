dee7849fd675910a273b6ee93d0f8a2911b87bb5
Daniel <daniel.g@sps.co.il>
2025-08-14 10:38:32 +0300

    fix: update .prettierrc printWidth and clean up NgCropper component imports and element references

--- libs/ng-cropper/src/lib/NgCropper/NgCropper.ts
+++ libs/ng-cropper/src/lib/NgCropper/NgCropper.ts
@@ -1,12 +1,5 @@
 import { JsonPipe } from '@angular/common';
-import {
-    ChangeDetectionStrategy,
-    Component,
-    CUSTOM_ELEMENTS_SCHEMA,
-    ElementRef,
-    viewChild,
-    viewChildren,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, CUSTOM_ELEMENTS_SCHEMA, ElementRef, viewChild, viewChildren } from '@angular/core';
 import {
     CropperCanvasElement,
     CropperCrosshairElement,
@@ -27,27 +20,14 @@ import {
     schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class NgCropper {
-    public readonly cropperCanvasRef = viewChild.required<ElementRef<CropperCanvasElement>>('cropperCanvas', {
-        debugName: 'cropperCanvasRef',
-    });
-    public readonly cropperImageRef = viewChild.required<ElementRef<CropperImageElement>>('cropperImage', {
-        debugName: 'cropperImageRef',
-    });
-    public readonly cropperShadeRef = viewChild.required<ElementRef<CropperShadeElement>>('cropperShade', {
-        debugName: 'cropperShadeRef',
-    });
-    public readonly cropperHandleRefs = viewChildren<ElementRef<CropperHandleElement>>('cropperHandle', {
-        debugName: 'cropperHandleRef',
-    });
-    public readonly cropperSelectionRef = viewChild.required<ElementRef<CropperSelectionElement>>('cropperSelection', {
-        debugName: 'cropperSelectionRef',
-    });
-    public readonly cropperGridRef = viewChild.required<ElementRef<CropperGridElement>>('cropperGrid', {
-        debugName: 'cropperGridRef',
-    });
-    public readonly cropperCrosshairRef = viewChild.required<ElementRef<CropperCrosshairElement>>('cropperCrosshair', {
-        debugName: 'cropperCrosshairRef',
-    });
+    // ================== Element References ==================
+    public readonly cropperCanvasRef = viewChild.required<ElementRef<CropperCanvasElement>>('cropperCanvas', { debugName: 'cropperCanvasRef' });
+    public readonly cropperImageRef = viewChild.required<ElementRef<CropperImageElement>>('cropperImage', { debugName: 'cropperImageRef' });
+    public readonly cropperShadeRef = viewChild.required<ElementRef<CropperShadeElement>>('cropperShade', { debugName: 'cropperShadeRef' });
+    public readonly cropperHandleRefs = viewChildren<ElementRef<CropperHandleElement>>('cropperHandle', { debugName: 'cropperHandleRefs' });
+    public readonly cropperSelectionRef = viewChild.required<ElementRef<CropperSelectionElement>>('cropperSelection', { debugName: 'cropperSelectionRef' });
+    public readonly cropperGridRef = viewChild.required<ElementRef<CropperGridElement>>('cropperGrid', { debugName: 'cropperGridRef' });
+    public readonly cropperCrosshairRef = viewChild.required<ElementRef<CropperCrosshairElement>>('cropperCrosshair', { debugName: 'cropperCrosshairRef' });
 
     constructor() {
         provideCropperJS().catch((err) => console.error('Cropper init failed', err));
